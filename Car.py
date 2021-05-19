203df7e4672683da1838b6ef49fe47477e246312
kris <kristismertina@gmail.com>
2021-05-19 20:26:53 +0300

    funсtion_type

--- Car.py
+++ Car.py
@@ -3,3 +3,14 @@ class Car_truck:
         self.type = type
         self.year_of_issue = year_of_issue
         self.weight = weight
+
+def type_truck (self):
+    if self.type ==  "Mersedes - Benz"  or "Reno":
+        return type
+    else:
+        print("Bed car")
+
+
+# h = Car_truck("Mersedes - Benz", 1996, 2000)
+# print (h.type)
+
