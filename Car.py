c8a9e708ad4abd9afe547ec661553b0f09c64979
kris <kristismertina@gmail.com>
2021-05-22 18:55:07 +0300

    class

--- Car.py
+++ Car.py
@@ -1,21 +1,8 @@
 class Car_truck:
     def __init__(self, type, max_over, min_over, weight_truck, travel_time):
         self.type = type
-<<<<<<< HEAD
         self.max_over = max_over
         self.min_over = min_over
         self.weight_truck = weight_truck
         self.travel_time = travel_time
 
-
-
-=======
-        self.year_of_issue = year_of_issue
-
-
-def type_truck (self):
-    if self.type ==  "Mersedes - Benz"  or "Reno":
-        return type
-    else:
-        print("Bed car")
->>>>>>> type_truck
