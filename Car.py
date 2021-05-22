2a12b3e581f85f10b6b3806ae4b0dfc571fb6c41
kris <kristismertina@gmail.com>
2021-05-22 14:30:54 +0300

    type_car

--- Car.py
+++ Car.py
@@ -2,4 +2,10 @@ class Car_truck:
     def __init__(self, type, year_of_issue, weight):
         self.type = type
         self.year_of_issue = year_of_issue
-        self.weight = weight
+
+
+def type_truck (self):
+    if self.type ==  "Mersedes - Benz"  or "Reno":
+        return type
+    else:
+        print("Bed car")
