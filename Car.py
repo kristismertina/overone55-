dd9b3ee0a0f5e5833b8a086dc71100d85b6d62e4
kris <kristismertina@gmail.com>
2021-05-22 19:00:46 +0300

    string

--- Car.py
+++ Car.py
@@ -6,3 +6,5 @@ class Car_truck:
         self.weight_truck = weight_truck
         self.travel_time = travel_time
 
+def __str__ (self):
+    return f"{self.max_over}, {self.min_over}, {self.weight_truck}, {self.travel_time} "
